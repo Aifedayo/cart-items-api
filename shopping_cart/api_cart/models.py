6c153af6f3aab8435c20524eefa86b8932f942cc
akeemlag <akeemifedayolag@gmail.com>
2021-10-13 14:00:00 +0100

    Updating cart commit

--- shopping_cart/api_cart/models.py
+++ shopping_cart/api_cart/models.py
@@ -4,3 +4,6 @@ class CartItem(models.Model):
     product_name = models.CharField(max_length=200)
     product_price = models.FloatField()
     product_quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.product_name
